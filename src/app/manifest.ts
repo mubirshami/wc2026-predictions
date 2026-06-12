ca43eb3935ee74c41fbc97ced2cb2c7c522fc4b5
mubirshami <mubirshami2000@gmail.com>
2026-06-13 02:54:16 +0500

    feat: add new icons for PWA manifest and update icon references

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,19 +12,19 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#10b981",
     icons: [
       {
-        src: "/icon.png",
+        src: "/icon-192.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/icon.png",
+        src: "/icon-512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/icon.png",
+        src: "/icon-512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
